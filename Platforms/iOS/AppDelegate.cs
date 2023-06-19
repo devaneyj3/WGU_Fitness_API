1d61354be2cf8a42247ee87781562673fa2daebc
jordandevaney <jordandevaney28@gmail.com>
2023-06-19 19:08:27 -0400

    Display classes to clients and view details to reserve classes

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Fit_Fitness_Client;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
